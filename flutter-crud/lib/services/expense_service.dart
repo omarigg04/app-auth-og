0bd555af9002ab84e17ca3611ab04c65d099b749
omarigg04 <gaxiolaomar04@gmail.com>
2025-07-16 11:56:24 -0600

    /expenses

--- flutter-crud/lib/services/expense_service.dart
+++ flutter-crud/lib/services/expense_service.dart
@@ -5,7 +5,7 @@ import 'auth_service.dart';
 
 class ExpenseService {
   // final String baseUrl = 'http://192.168.1.117:3000/expenses';
-    final String baseUrl = 'http://localhost:3000'; // Para emulador iOS o navegador
+    final String baseUrl = 'http://localhost:3000/expenses'; // Para emulador iOS o navegador
 
   final AuthService _authService = AuthService();
 
