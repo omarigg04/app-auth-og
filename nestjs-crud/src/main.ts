5c866e7e562676dc629cb8a995a972282abb3ff7
omarigg04 <gaxiolaomar04@gmail.com>
2025-07-17 00:51:32 -0600

    adding patch to cors policy

--- nestjs-crud/src/main.ts
+++ nestjs-crud/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: '*', // o restringido a IP de tu cel si prefieres
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   });
 
   await app.listen(process.env.PORT ?? 3000);
