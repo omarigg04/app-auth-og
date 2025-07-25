1ac911348b281de61c736e2f483fd94dc94a8d95
omarigg04 <gaxiolaomar04@gmail.com>
2025-07-25 11:41:30 -0600

    adding vercel config

---
+++ nestjs-crud/api/index.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+let app: any;
+
+async function bootstrap() {
+  if (!app) {
+    app = await NestFactory.create(AppModule);
+    
+    app.enableCors({
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    });
+    
+    await app.init();
+  }
+  return app;
+}
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const app = await bootstrap();
+  return app.getHttpAdapter().getInstance().handle(req, res);
+}
